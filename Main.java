3b72184349acf8207764e9a67782fd126faaa74c
Ubaldo <ubaldocapalbo@gmail.com>
2023-12-02 14:37:32 +0100

    end

--- Main.java
+++ Main.java
@@ -2,8 +2,9 @@ import java.util.HashSet;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("dimensione " + newFullHash(7).size());
-        System.out.println("elementi " + newFullHash(7));
+        HashSet<Integer> hash1 = newFullHash(7);
+        System.out.println(hash1.size());
+        System.out.println(hash1);
     }
     public static HashSet<Integer> newFullHash (int numElementi) {
         HashSet<Integer> nuovo = new HashSet<>();
